f320ee5bbf41d3e3ede16cbd0128b2cdc40fb508
DHeijbroek <diederik.heijbroek@gmail.com>
2023-05-26 11:19:04 +0200

    adapt video extraction code

--- TikTokPy-main/download_videos.py
+++ TikTokPy-main/download_videos.py
@@ -3,14 +3,19 @@ import io
 import glob
 import os
 import urllib.request
-from os import path
 from pathlib import Path
+import pandas as pd
+import numpy as np
 
 import aiohttp
 from tiktokapipy.async_api import AsyncTikTokAPI
 from tiktokapipy.models.video import Video
 
 directory = 'videos'
+video_data_file = 'product1.csv'
+video_tag = 'ctilbury'
+url_list = []
+video_id_list = []
 
 async def save_slideshow(video: Video):
     # this filter makes sure the images are padded to all the same size
@@ -21,9 +26,9 @@ async def save_slideshow(video: Video):
     for i, image_data in enumerate(video.image_post.images):
         url = image_data.image_url.url_list[-1]
         # this step could probably be done with asyncio, but I didn't want to figure out how
-        urllib.request.urlretrieve(url, path.join(directory, f"temp_{video.id}_{i:02}.jpg"))
+        urllib.request.urlretrieve(url, os.path.join(directory, f"temp_{video.id}_{i:02}.jpg"))
 
-    urllib.request.urlretrieve(video.music.play_url, path.join(directory, f"temp_{video.id}.mp3"))
+    urllib.request.urlretrieve(video.music.play_url, os.path.join(directory, f"temp_{video.id}.mp3"))
 
     # use ffmpeg to join the images and audio
     command = [
@@ -46,14 +51,14 @@ async def save_slideshow(video: Video):
     _, stderr = await ffmpeg_proc.communicate()
     generated_files = glob.glob(path.join(directory, f"temp_{video.id}*"))
 
-    if not path.exists(path.join(directory, f"temp_{video.id}.mp4")):
+    if not os.path.exists(os.path.join(directory, f"temp_{video.id}.mp4")):
         # optional ffmpeg logging step
         # logging.error(stderr.decode("utf-8"))
         for file in generated_files:
             os.remove(file)
         raise Exception("Something went wrong with piecing the slideshow together")
 
-    with open(path.join(directory, f"temp_{video.id}.mp4"), "rb") as f:
+    with open(os.path.join(directory, f"temp_{video.id}.mp4"), "rb") as f:
         ret = io.BytesIO(f.read())
 
     for file in generated_files:
@@ -72,7 +77,7 @@ async def save_video(video: Video, api: AsyncTikTokAPI):
 async def download_video(url, tag):
     # mobile emulation is necessary to retrieve slideshows
     # if you don't want this, you can set emulate_mobile=False and skip if the video has an image_post property
-    async with AsyncTikTokAPI(emulate_mobile=True) as api:
+    async with AsyncTikTokAPI(emulate_mobile=True, navigation_timeout=120.0, navigation_retries=3) as api:
         
         # Check if the directory exists
         if not os.path.exists(os.path.join(directory, tag)):
@@ -82,6 +87,15 @@ async def download_video(url, tag):
             os.makedirs(os.path.join(directory, tag, 'desc'))
         
         video: Video = await api.video(url)
+        url_list.append(url)
+        video_id_list.append(video.id)
+        
+        # check if video already exists
+        if os.path.join(directory, tag, str(video.id) + '.mp4'):
+            print(f'already processed {video.id}')
+            return
+        print(f'processing {video.id} of url {url}')
+        
         # print(video)
         if video.image_post:
             # downloaded = await save_slideshow(video)
@@ -103,18 +117,21 @@ async def download_video(url, tag):
             f = open(f'{directory}/{tag}/desc/{video.id}_description.txt',"w")
             f.write(video.desc)
             f.close()
-            
-async def do_something():
-    async with AsyncTikTokAPI(navigation_timeout=120.0, navigation_retries=3) as api:
-        tiktok_tag = 'ctilburypartner'
-        challenge = await api.challenge(tiktok_tag, video_limit=100)
+    
         
-        # print(inspect.getmembers(api))
-        async for video in challenge.videos:
-            # print(video.is_ad)
-            link = f'https://www.tiktok.com/@{video.author}/video/{video.id}'
-            
-            await download_video(link, tiktok_tag)
+async def extract_tiktoks():
+    
+    video_data = pd.read_csv(os.path.join(directory, video_data_file), sep = ';')
+        
+    # iterate over the urls to extract the videos
+    for index, row in video_data.iterrows():
+        link = row.URL   
+        try:     
+            await download_video(link, video_tag)
+        except:
+            print(f'could not load {link}')
+        
+    pd.DataFrame(np.transpose([url_list, video_id_list]), columns=['urls', 'video_ids']).to_csv('output.csv')
     
 # run subroutine    
-asyncio.run(do_something())
+asyncio.run(extract_tiktoks())
